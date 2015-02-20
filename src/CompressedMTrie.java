3f428725cd0a475248bf8fddc6093fc4b0272e0e
mnm364 <mnm364@users.noreply.github.com>
2015-02-19 21:13:24 -0500

    working on Trie

--- src/CompressedMTrie.java
+++ src/CompressedMTrie.java
@@ -9,21 +9,62 @@ public class CompressedMTrie {
 	private DoubleHashedHashMap<TrieNode> root;
 	
 	private final class TrieNode {
+		
+		/* character to be used in Trie. */
 		private char val;
-		private boolean end;
+		
+		/* mark leaf nodes. */
+		private boolean leaf;
+		
+		/* ??identify if compressed string node 
+		 * This does not make sense to be here but a good reminder to deal with compression
+		 */
 		private boolean str;
+		
+		/* pointer? to subsequent hash map */
 		private DoubleHashedHashMap<TrieNode> child;
+		
 		//TODO - make this point to an Entry
+		/* list of pointers to unordered master Set */
 		private Set<Entry> eSet;
 		
-		private TrieNode(char val, boolean end, boolean str, DoubleHashedHashMap<TrieNode> child, Entry e) {
+		private TrieNode(char val, boolean leaf, boolean str, DoubleHashedHashMap<TrieNode> child, Entry e) {
 			this.val	= val;
-			this.end	= end;
+			this.leaf	= leaf;
 			this.str	= str;
 			this.child	= child;
 			
 			this.eSet.add(e);
 		}
 		
+	}
+	
+	/**
+	 * 
+	 * @param str the string to insert
+	 * @return true if insert successful; false otherwise
+	 */
+	public boolean insert(String str) {
+		//TODO - method stub
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public boolean remove(String str, float id) {
+		//TODO - method stub
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private boolean compress() {
+		//TODO - method stub
+		return false;
 	}
 }
