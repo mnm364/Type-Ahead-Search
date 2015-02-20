4125663156f3a6f3a0b303b5f53df2d0af701ca4
mnm364 <mnm364@users.noreply.github.com>
2015-02-19 19:22:18 -0500

    Entry class update

--- src/Entry.java
+++ src/Entry.java
@@ -1,4 +1,17 @@
 
 public class Entry {
-	int i;
+	
+	private String id;
+	private char type;
+	private float score;
+	
+	/* Constructor */
+	public Entry(String id, char type, float score) {
+		/* initialize values */
+		this.id 	= id;
+		this.type 	= type;
+		this.score	= score;
+	}
+	
+	
 }
