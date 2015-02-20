47fe210e750819f0a568de43830ca795117ea9a5
mnm364 <mnm364@users.noreply.github.com>
2015-02-19 19:42:25 -0500

    Entry comments

--- src/Entry.java
+++ src/Entry.java
@@ -1,17 +1,26 @@
-
+/*
+ * 
+ */
 public class Entry {
 	
+	/* Unique ID */
 	private String id;
+	
+	/* Entry type (user | topic | question | board) */
 	private char type;
+	
+	/* Score given to the entry to weight results */
 	private float score;
 	
-	/* Constructor */
+	/* Constructor 
+	 * @param id the unique id
+	 * @param type (user | topic | question | board) 
+	 * @param score used to weight results
+	 */
 	public Entry(String id, char type, float score) {
-		/* initialize values */
+	//	initialize values
 		this.id 	= id;
 		this.type 	= type;
 		this.score	= score;
 	}
-	
-	
 }
